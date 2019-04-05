78a5a818b493557a2d0d9dd4265aa0eb1c40e321
Paulina Jaworska <paulinajaworska9@gmail.com>
2019-04-05 20:11:04 +0200

    Players can play with X and O. Box to put signs in are not overwriting each other.

--- server.py
+++ server.py
@@ -18,30 +18,3 @@ def game():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-"""
-/*var ticTacToe = _.range(3).map(function () {
- *       // Create one row
- *       return _.range(3).map(function () {
- *           return '.';
- *       });
- *   });
- *   ticTacToe[0][2] = 'X';  // [row][column]
- *   ticTacToe.forEach(function (row) {
- *       console.log(row.join(' '));
- *   });
- *
- *
- *
- *function createArray(length) {
- *    var arr = new Array(length || 0),
- *      i = length;
- *
- *   if (arguments.length > 1) {
- *       var args = Array.prototype.slice.call(arguments, 1);
- *       while(i--) arr[length-1 - i] = createArray.apply(this, args);
- *   }
- *
- *   return arr;
- *}*/"""
